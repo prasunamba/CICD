a7ab963f529405418dac0fdd9b3a17cb30a801be
prasuna <prassna44@gmail.com>
2024-12-11 17:53:42 +0530

    added the gin

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func handlerfunc(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "your ping reached here",
+	})
+
+}
 func main() {
-	fmt.Println("hello prasssu's world")
+	r := gin.Default()
+	r.GET("/ping", handlerfunc)
+	r.Run()
 }
